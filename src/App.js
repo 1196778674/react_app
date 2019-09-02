090c393a8f910d47ba0ecacaee3e43e288c9f96b
liufei <feiliu@58coin.com>
2019-09-02 22:00:15 +0800

    精简 ‘

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
-export default class App extends Component {
+class App extends PureComponent {
   constructor(props) {
-    super(props);
+    super(props)
+
     this.state = {
-      list: [1,2,3,4,5,6],
-      items: ''
+      
     }
-    this.handlerChange = this.handlerChange.bind(this)
-  }
-  handlerChange () {
-    this.setState({
-      items: '123'
-    })
-  }
-  componentWillMount() {
-    let items = this.state.list.map((key) => {
-      return <li className="" key={key}>{key}</li>
-    })
-    this.setState({
-      items: items
-    })
   }
 
-  componentDidMount() {
-    
-  }
-
-  componentWillUnmount() {
-    
-  }
-  
-  
   render() {
     return (
       <div>
-        {this.state.items}
-        <button onClick={this.handlerChange}>change</button>
+      test
       </div>
     )
   }
 }
 
+export default App
